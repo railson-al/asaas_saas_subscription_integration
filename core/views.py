75f6394283dc739dc3c9f823f00c8dc9637569bc
Railson Almeida <71390605+railson-al@users.noreply.github.com>
2026-03-06 12:49:52 -0300

    auth

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "is_active": user.is_active,
+            "date_joined": user.date_joined
+        })
